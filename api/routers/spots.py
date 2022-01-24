dbf1ac4e88912dd29221b1e9a29d9c93fb569e64
Mark Aurelius <tmalone@protonmail.ch>
2022-01-24 03:52:37 +0000

    Imported async db connection. Finished the index route

--- api/routers/spots.py
+++ api/routers/spots.py
@@ -6,8 +6,10 @@ from api import db
 from api import config
 
 
+db = db.async_detabase()
 router = fastapi.APIRouter(prefix='/spots', tags=["Spots"])
 
 @router.get('/')
 async def fetch_all() -> list:
-    
+    result = db.fetch()
+    return result.items
